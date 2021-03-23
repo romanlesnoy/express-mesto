522c4226d4ac433080fa01395012f4ab44331af8
Roman Lesnoy <romanlesnoy@outlook.com>
2021-03-23 19:26:15 +0300

    add email, password field into userSchema, hash password with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,21 +1,22 @@
 const { Schema, model } = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    default: "Жак-Ив Кусто",
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
+    default: "Исследователь",
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
+    default: "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
     validate: {
       validator(v) {
         const regExpForUelValidate = /^((http|https):\/\/)(www\.)?([\w\W\d]{1,})(\.)([\w\W\d]{1,})$/g;
@@ -23,6 +24,23 @@ const userSchema = new Schema({
       },
       message: 'Введите корректную ссылку',
     },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return isEmail(v);
+      },
+      message: 'Введите корректную ссылку',
+    },
+  },
+  password: {
+    type: String,
+    default: "Жак-Ив Кусто",
+    required: true,
+    minlength: 8,
   },
 });
 
