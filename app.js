721105953079f923b5faee91f8b06d05b66590a5
Roman Lesnoy <romanlesnoy@outlook.com>
2021-03-27 16:47:48 +0300

    check ssh-key

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createProfile } = require('./controllers/users');
 
-console.log()
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
